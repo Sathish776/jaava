1a32b393d1ccb8002267b42fd45f40736e994db2
Sathish776 <satheshkumar66.sk@gmail.com>
2022-08-20 13:19:33 +0530

    add on

---
+++ src/inherit/Inherit.java
@@ -0,0 +1,10 @@
+package inherit;
+
+public class Inherit {
+	String FatherName="chandrasekar";
+	String MotherName="buvaneshwari";
+	public void fatherDetails() {
+		System.out.println("FatherName= "+FatherName);
+		System.out.println("MotherName= "+MotherName);}
+
+}
